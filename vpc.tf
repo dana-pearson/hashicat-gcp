4386edb09127daf16c9eb06e452e36853099ad2d
Dana Pearsone <dana_pearson@stratascale.com>
2023-08-03 21:32:29 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.4"
 
-    project_id   = "var.project"
+    project_id   = var.project
     network_name = "gaurav-subnet"
     routing_mode = "GLOBAL"
 
