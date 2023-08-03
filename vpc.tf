7108a60d20f4d5723880a64c20d4c8803d35a318
dana-pearson <88057351+dana-pearson@users.noreply.github.com>
2023-08-03 17:11:05 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.4"
 
-    project_id   = "p-zjufp7qphhnw-0"
+    project_id   = "var.project"
     network_name = "gaurav-subnet"
     routing_mode = "GLOBAL"
 
